de6868ecac473b20fbe5fcf7121298571217757e
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-05-18 14:20:20 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "my_aws_instance" {
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
   associate_public_ip_address = true
-  key_name               = var.key_name
+  #key_name               = var.key_name
 
   root_block_device {
     volume_size = 15
